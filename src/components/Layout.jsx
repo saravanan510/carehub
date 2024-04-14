050d0d011e8c46e5485eb58a09b29e8dd03b4ee3
saravanan510 <saravanan5101998@gmail.com>
2024-04-14 13:20:08 +0530

    new commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import ScrollToTop from "./ScrollTop";
 
 import "animate.css/animate.css";
 
 const Layout = () => {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
--- src/pages/PrivateNursing.jsx
+++ src/pages/PrivateNursing.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import PrivateNursing_Banner from "../assets/PrivateNursing-Banner.jpg";
 import ServicePage_list from "../components/ServicePage_list";
 import ServicePage_details from "../components/ServicePage_details";
+import ScrollToTop from "../components/ScrollTop";
 const PrivateNursing = () => {
   return (
     <>
+      <ScrollToTop />
       <div className="servicepage_banner">
         <h2 className="fs-1 fw-semibold text-white">Private Nursing</h2>
       </div>
